4eb05cce476b006d5abd0d51ff74ccb47f838baa
Argenis <argenistola@hotmail.com>
2018-11-27 11:05:50 -0500

    update evaluacionfuerza tecnica y horarios entrenamientos

---
+++ sidma/storage/framework/views/25a9be51df8dcff4e11ecf00f0285a58d0f23d94.php
@@ -0,0 +1,9 @@
+<footer>
+          <div class="pull-right">
+            Sidma - Derechos reservados <a href="https://fedemanabi.com">FEDEMANABI</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
