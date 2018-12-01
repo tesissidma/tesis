126b67b3e32c365c8cac82af4817ea4213bc6896
Argenis <argenistola@hotmail.com>
2018-12-01 10:17:58 -0500

    busqueda de atleta en modal para controles competencias

--- sidma/resources/views/layout/informacionperfil.blade.php
+++ sidma/resources/views/layout/informacionperfil.blade.php
@@ -3,7 +3,7 @@
                 <img src="images/img.jpg" alt="..." class="img-circle profile_img">
               </div>
               <div class="profile_info">
-                <span>Welcome,</span>
+                <span>Bienvenido.</span>
                 <h2>John Doe</h2>
               </div>
             </div>
--- sidma/resources/views/metodologo/form/modalbusquedaAtletas.blade.php
+++ sidma/resources/views/metodologo/form/modalbusquedaAtletas.blade.php
@@ -5,10 +5,45 @@
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
+        <h4 class="modal-title">Busqueda de Atleta</h4>
       </div>
+
       <div class="modal-body">
-        <br>
-          
+          <br>
+
+          <div class="row">
+          <div class="col-md-7 col-sm-12 col-xs-12 form-group">
+                    
+                    <input type="text" placeholder="Ingrese Nombres del Atleta" class="form-control">
+
+          </div>
+          <button class="btn btn-success my-2 my-sm-0" type="submit">Buscar</button>
+        </div>
+
+        <table id="tiemposymarcas" class="table table-bordered table-responsive-md table-striped text-center" style="text-align: center;">
+                      <thead>
+                        <tr>
+                          <th style="text-align: center; vertical-align:middle;">N°</th>
+                          <th style="text-align: center; vertical-align:middle;">Nombres y Apellidos</th>
+                          
+                          <th ROWSPAN="2" style="text-align: center; vertical-align:middle;">Acción</th>
+                          
+                        </tr>
+                        
+                      </thead>
+                      <tbody>
+
+
+                       <tr>
+                          <td style="text-align: center; vertical-align:middle;" class="pt-3-half">1</td>
+                          <td style="text-align: center; vertical-align:middle;" class="pt-3-half">Guadalupe House Guadalupe House</td>
+                          <td style="text-align: center; vertical-align:middle;"class="pt-3-half"><button type="button" class="btn btn-success btn-rounded btn-sm my-0">Agregar Atleta</button></td>
+                </tr>
+
+                        
+                      </tbody>
+                    </table>
+     
          
                  
             
